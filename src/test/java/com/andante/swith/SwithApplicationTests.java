cb3babf5ddb12183ab0d20bef6bc7bb43b548129
5sm <74877462+sangmin970528@users.noreply.github.com>
2021-10-27 19:30:16 +0900

    init

---
+++ src/test/java/com/andante/swith/SwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andante.swith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
