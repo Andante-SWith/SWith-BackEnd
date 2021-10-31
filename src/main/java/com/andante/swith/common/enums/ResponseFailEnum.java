dbdf730a7fcaa2922cad003140cdfeea21f87acb
5sm <74877462+sangmin970528@users.noreply.github.com>
2021-10-31 15:32:23 +0900

    add common

--- src/main/java/com/andante/swith/common/enums/ResponseFailEnum.java
+++ src/main/java/com/andante/swith/common/enums/ResponseFailEnum.java
@@ -1,4 +1,22 @@
 package com.andante.swith.common.enums;
 
-public class ResponseFailEnum {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseFailEnum implements EnumType {
+
+    NO_DATA("-1", "사용자 정보를 찾을 수 없습니다."),
+    SERVER_ERROR("-2", "서버 에러입니다."),
+    LOGIN_ERROR("-3", "이메일 또는 비밀번호가 틀립니다.");
+
+
+    private String code;
+    private String message;
+
+    @Override
+    public String getId() {
+        return name();
+    }
 }
