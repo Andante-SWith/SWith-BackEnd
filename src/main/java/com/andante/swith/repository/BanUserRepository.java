0fdc760194edcc4490ccb358f9dae41b6e6253a2
5sm <74877462+sangmin970528@users.noreply.github.com>
2021-12-02 23:32:56 +0900

    Update BanUserRepository.java

--- src/main/java/com/andante/swith/repository/BanUserRepository.java
+++ src/main/java/com/andante/swith/repository/BanUserRepository.java
@@ -1,7 +1,10 @@
 package com.andante.swith.repository;
 
 import com.andante.swith.entity.BanUser;
+import com.andante.swith.entity.Studyroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BanUserRepository extends JpaRepository<BanUser,Long> {
   
